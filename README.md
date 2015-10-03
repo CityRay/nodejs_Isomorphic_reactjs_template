a880fbd6b763310baf4a27aba052758110532e47
RayLin <raylin@RayLindeMacBook-Pro.local>
2015-10-04 02:01:21 +0800

    create project

---
+++ README.md
@@ -0,0 +1,18 @@
+# NODE.JS & Isomorphic React.JS Template
+
+
+# Framework
+
+- [Express]
+- [React.js]
+- [Webpack]
+
+
+# Start
+
+```
+$ npm install
+$ npm start
+```
+
+Open browswer http://localhost:3000
---
+++ src/shared/components/AppController.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class AppController extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>NODE.JS & Isomorphic React.JS Template</h1>
+            <p className="text">HI NODE.JS & Isomorphic React.JS Template</p>
+        </div>
+        );
+  }
+}
